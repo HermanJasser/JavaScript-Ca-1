4a4aeefa0596d51586dd90cbd78666011658b83d
HermanJasser <herjas00989@stud.noroff.no>
2024-02-15 09:24:12 +0100

    fått til handlekurv

--- js/spillside.js
+++ js/spillside.js
@@ -1,59 +1,62 @@
-import {listHandlekurv} from "./utils.js";
+import { listHandlekurv } from "./utils.js";
 
 let mainSpillSide = document.querySelector("main.main-spillside");
-let spillApiById =[];
+let spillApiById = [];
 
 //console.log(mainSpillside);
-
 
 let params = new URL(document.location).searchParams;
 let id = params.get("id");
 
 let ref = document.referrer;
 
-async function getApiById(){
-    try{
-        const api = `https://v2.api.noroff.dev/gamehub/${id}`;
-        const response = await fetch(api);
-        const data = await response.json();
-    
-        spillApiById = data.data;
-        //console.log(spillApiById);
-        lagSpillSide(spillApiById, mainSpillSide)
-    } catch(error){
-        mainSpillSide.innerHTML = `<p>Kan ikke finne spillet du ser etter</p>`
-    }
+async function getApiById() {
+  try {
+    const api = `https://v2.api.noroff.dev/gamehub/${id}`;
+    const response = await fetch(api);
+    const data = await response.json();
+
+    spillApiById = data.data;
+    //console.log(spillApiById);
+    lagSpillSide(spillApiById, mainSpillSide);
+    addEventListener(id);
+  } catch (error) {
+    mainSpillSide.innerHTML = `<p>Kan ikke finne spillet du ser etter</p>`;
+  }
 }
 getApiById();
 
-function lagSpillSide(api, output){
-    let spillSideInput =`
-    <div class="spillside-left">
-    <p><a class="tilbake-knapp" href="${ref}">Tilbake</a></p>
-    <h1>${api.title}</h1>
-    <img src="${api.image.url}" alt="${api.image.alt}">
-    </div>
+function lagSpillSide(api, output) {
+  output.innerHTML = `
+  <div class="spillside-left">
+  <p><a class="tilbake-knapp" href="${ref}">Tilbake</a></p>
+  <h1>${api.title}</h1>
+  <img src="${api.image.url}" alt="${api.image.alt}">
+  </div>
 
-    <div class="spillside-right">
-        <div class="pris-div">
-            <p class="nå-pris">${api.price} $</p>
-        </div>
-        <a class="legg-i-handlekurv" id=${api.id} href="#">Legg til i handlekurv</a>
-        <section class="produkt-beskrivelse">
-            <h2>Produkt beskrivelse</h2>
-            <p>${api.description}</p>
-            <h2>Spesifikasjoner</h2>
-            <ul>
-                <li>Aldersgrense: ${api.ageRating}</li>
-                <li>Sjanger: ${api.genre}</li>
-                <li>Gitt ut i ${api.released}</li>
-            </ul>
-        </section>
-    </div>`;
-
-    output.innerHTML = spillSideInput;
+  <div class="spillside-right">
+      <div class="pris-div">
+          <p class="nå-pris">${api.price} $</p>
+      </div>
+      <a class="legg-i-handlekurv" id=${api.id} href="#">Legg til i handlekurv</a>
+      <section class="produkt-beskrivelse">
+          <h2>Produkt beskrivelse</h2>
+          <p>${api.description}</p>
+          <h2>Spesifikasjoner</h2>
+          <ul>
+              <li>Aldersgrense: ${api.ageRating}</li>
+              <li>Sjanger: ${api.genre}</li>
+              <li>Gitt ut i ${api.released}</li>
+          </ul>
+      </section>
+  </div>`;
 }
 
-
-    
+function addEventListener(id) {
+  const handleKurvKnapp = document.querySelector(".legg-i-handlekurv");
+  handleKurvKnapp.addEventListener("click", (e) => {
+    e.preventDefault();
+    listHandlekurv(id);
+  });
+}
 
--- js/utils.js
+++ js/utils.js
@@ -1,63 +1,43 @@
-
-
 export function listProductCards(api, output) {
     output.innerHTML = "";
     let spillContainer = "";
-    for(let product of api){
-        spillContainer +=`<a class="link-spill-container" href="./spillside.html?id=${product.id}"><div class="spill-container">
-                <img src="${product.image.url}" alt="${product.image.alt}">
-                <p>${product.title}</p>
-                <p class="pris">${product.price} $</p>
-            </div></a>`
+    for (let product of api) {
+      spillContainer += `<a class="link-spill-container" href="./spillside.html?id=${product.id}"><div class="spill-container">
+                  <img src="${product.image.url}" alt="${product.image.alt}">
+                  <p>${product.title}</p>
+                  <p class="pris">${product.price} $</p>
+              </div></a>`;
     }
     output.innerHTML = spillContainer;
-};
-
-
-/*************
- handlekurvkode:(
- *************/
-
-    export let handlekurv;
-
-    export const lagringHandlekurv = localStorage.getItem("handlekurven");
-    if (!lagringHandlekurv) {
-        handlekurv = [];
-        localStorage.setItem("handlekurven", JSON.stringify(handlekurv));
-    } else {
-        handlekurv = JSON.parse(lagringHandlekurv);
-    }
-
-    const leggTil = document.querySelectorAll(".legg-i-handlekurv");
-
-    for (let add of leggTil) {
-    /* if (handlekurv.includes(add.id){
-
-    } */
-    add.addEventListener("click", (e)=> {
-        e.preventDefault();
-        listHandlekurv()
-        console.log("test")
-    })
-    };
+  }
   
-    export function listHandlekurv() {
-        console.log("kjører")
-        console.log(handlekurv)
-        console.log(handlekurv.includes(this.id))
-        if (handlekurv.includes(this.id)) {
-            for (let i = 0; i < handlekurv.length; i++) {
-                if (handlekurv[i] === this.id) handlekurv.splice(i, 1); 
-            }
-        } else {
-            handlekurv.push(this.id);
-            console.log(handlekurv)
-        }
-
-        localStorage.setItem("handlekurven", JSON.stringify(handlekurv));
+  /*************
+   handlekurvkode:(
+   *************/
+  
+  export let handlekurv;
+  
+  export const lagringHandlekurv = localStorage.getItem("handlekurven");
+  if (!lagringHandlekurv) {
+    handlekurv = [];
+    localStorage.setItem("handlekurven", JSON.stringify(handlekurv));
+  } else {
+    handlekurv = JSON.parse(lagringHandlekurv);
+  }
+  
+  export function listHandlekurv(id) {
+    let added = false;
+    if (handlekurv.includes(id)) {
+      added = true;
     }
-
-    
-    
-    console.log(lagringHandlekurv);
+    console.log("kjører");
+    console.log(handlekurv.includes(id));
+    if (added) {
+      handlekurv = handlekurv.filter((item) => item !== id);
+    } else {
+      handlekurv.push(id);
+    }
+    localStorage.setItem("handlekurven", JSON.stringify(handlekurv));
     console.log(handlekurv);
+  }
+  ﻿
